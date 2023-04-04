4ff624353d4267bfb057632ab0d5c15c8587ea18
Utsav Joshi <utsavjoshi236@gmail.com>
2023-04-04 14:40:31 +0530

    personal Portfolio

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-console.log("Hello World");
-console.warn("Heuuu")
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "shubham607joshi@gmail.com",
+        Password : "kisnautsav",
+        To : 'shubham607joshi@gmail.com',
+        From : document.getElementById("email").value,
+        Subject :document.getElementById("lttr").value,
+        Body : "And this is the body"
+    }).then(
+    message => alert(message)
+    );
+}
+let typed = new Typed('#element', {
+    strings: ["Utsav here", "WebApp developer"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+    fadeOut: false,
+  fadeOutClass: 'typed-fade-out',
+  fadeOutDelay: 500,
+  });
