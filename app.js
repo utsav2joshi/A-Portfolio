bff5567be584b66dd43b800ff2714057807e39ec
utsav2joshi <101290008+utsav2joshi@users.noreply.github.com>
2023-04-04 14:49:21 +0530

    Update app.js
    This function isn't working

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-function sendEmail(){
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "shubham607joshi@gmail.com",
-        Password : "kisnautsav",
-        To : 'shubham607joshi@gmail.com',
-        From : document.getElementById("email").value,
-        Subject :document.getElementById("lttr").value,
-        Body : "And this is the body"
-    }).then(
-    message => alert(message)
-    );
-}
 let typed = new Typed('#element', {
     strings: ["Utsav here", "WebApp developer"],
     typeSpeed: 60,
